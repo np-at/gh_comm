0abcbf4536863e4fc5cef623aba90ae888ccddf0
Noah Praskins <noahpraskins@gmail.com>
2022-04-08 20:48:27 -0700

    quickfix to layout constraint issues

--- pages/_app.page.tsx
+++ pages/_app.page.tsx
@@ -1,22 +1,38 @@
-import '../styles/globals.css'
-import type {AppProps} from 'next/app'
-import {NextPage} from "next";
-import {ReactElement, ReactNode} from "react";
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { NextPage} from "next";
+import React, { ReactElement, ReactNode, useEffect } from "react";
 import Layout from "@components/Layout/Layout";
 
-
 export type NextPageWithLayout<T> = NextPage<T> & {
-    getLayout?: (page: ReactElement) => ReactNode
-}
+  getLayout?: (page: ReactElement) => ReactNode;
+};
 type AppPropsWithLayout = AppProps & {
-    Component: NextPageWithLayout<any>
+  Component: NextPageWithLayout<any>;
+};
+
+
+function App({ Component, pageProps }: AppPropsWithLayout) {
+  useEffect(() => {
+    // @ts-ignore
+    window.netlifyIdentity?.on("init", (user) => {
+      if (!user) {
+        // @ts-ignore
+        window.netlifyIdentity.on("login", () => {
+          document.location.href = "/admin/";
+        });
+      }
+    });
+  }, []);
+  // Use the layout defined at the page level, if available
+  const getLayout = Component.getLayout;
+  return (
+    (getLayout && getLayout(<Component {...pageProps} />)) || (
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    )
+  );
 }
 
-function App({Component, pageProps}: AppPropsWithLayout) {
-
-    // Use the layout defined at the page level, if available
-    const getLayout = Component.getLayout;
-    return (getLayout && getLayout(<Component {...pageProps} />)) || <Layout><Component {...pageProps} /></Layout>
-}
-
-export default App
+export default App;
